c179e5039f82c8699479e5f7de9c5557e947ae6d
Sidney Cardoso <sidneyrsc342@gmail.com>
2022-09-30 23:07:39 -0300

    JWT de usuários

---
+++ src/controllers/Token.js
@@ -0,0 +1,40 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/User';
+
+class TokenController {
+  async store(req, res) {
+    try {
+      const { email = '', password = '' } = req.body;
+
+      if (!email || !password) {
+        return res.status(401).json({
+          errors: ['Invalid credentials'],
+        });
+      }
+      const user = await User.findOne({ where: { email } });
+      if (!user) {
+        return res.status(401).json({
+          errors: ['User not found'],
+        });
+      }
+
+      if (!(await user.passwordIsValid(password))) {
+        return res.status(401).json({
+          errors: ['Invalid password'],
+        });
+      }
+      const { id } = user;
+      const secret = process.env.TOKEN_SECRET;
+      const tokenExpiration = {
+        expiresIn: process.env.TOKEN_EXPIRATION,
+      };
+      const token = jwt.sign({ id, email }, secret, tokenExpiration);
+
+      return res.json({ token });
+    } catch (error) {
+      return res.status(400).json(error.errors.map(err => err.message));
+    }
+  }
+}
+
+export default new TokenController();
