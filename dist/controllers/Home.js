b4a198537ab180fa52622be4a58540136e9bae54
Sidney Cardoso <62916279+sidney-cardoso@users.noreply.github.com>
2022-11-04 12:03:26 -0300

    Delete dist directory

--- dist/controllers/Home.js
+++
@@ -1,7 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});class HomeController {
-  async index(req, res) {
-    return res.json('Index');
-  }
-}
-
-exports. default = new HomeController();
