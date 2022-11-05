59b740293e728d4455f08fda9ad34b1bde3655ef
Sidney Cardoso <sidneyrsc342@gmail.com>
2022-11-05 09:47:42 -0300

    Correção de importação express-delay

--- dist/app.js
+++ dist/app.js
@@ -3,7 +3,7 @@ var _dotenv = require('dotenv'); var _dotenv2 = _interopRequireDefault(_dotenv);
 var _path = require('path');
 var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
 // import helmet from 'helmet';
-// import { delay } from 'express-delay';
+var _expressdelay = require('express-delay'); var _expressdelay2 = _interopRequireDefault(_expressdelay);
 
 var _home = require('./routes/home'); var _home2 = _interopRequireDefault(_home);
 var _user = require('./routes/user'); var _user2 = _interopRequireDefault(_user);
@@ -37,7 +37,7 @@ class App {
   middlewares() {
     this.app.use(_cors2.default.call(void 0, corsOptions));
     // this.app.use(helmet());
-    // this.app.use(delay(2000));
+    this.app.use(_expressdelay2.default.call(void 0, 2000));
     this.app.use(_express2.default.urlencoded({ extended: true }));
     this.app.use(_express2.default.json());
     this.app.use(
